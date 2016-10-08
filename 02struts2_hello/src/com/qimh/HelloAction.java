b2337fcc5350c2f76284123808cea8bc9444d6c5
qimh2 <2237870348@qq.com>
2016-10-08 14:30:38 +0800

    添加struts2 两个demo

---
+++ 02struts2_hello/src/com/qimh/HelloAction.java
@@ -0,0 +1,10 @@
+package com.qimh;
+
+public class HelloAction {
+	
+	public String execute(){
+		System.out.println("hello struts2");
+		return "success";
+	}
+
+}
---
+++ 02struts2_login/src/com/qimh/LoginAction.java
@@ -0,0 +1,37 @@
+package com.qimh;
+
+public class LoginAction {
+	
+	
+	private String username ;
+	private String pwd;
+	
+	
+	//处理方法
+	public String execute(){
+		
+		if("qimh".equals(username) && "123456".equals(pwd)){
+			
+			return "success";
+		}else{
+			
+			return "failed";
+		}
+	}
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+
+}
